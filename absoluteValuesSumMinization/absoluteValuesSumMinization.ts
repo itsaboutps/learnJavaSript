a023b9a9c8ed98e41feb67d2a15a201d851a7f89
prashant_nv <psprashantsingh91@gmail.com>
2022-06-18 13:08:18 +0530

    algorithms-1

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -3,10 +3,16 @@ export function absoluteValuesSumMinimization(a: number[]): number {
     return isEven?a[a.length/2 -1] : a[Math.floor(a.length/2)]
 
     
-    
+
 }
 
 console.log(absoluteValuesSumMinimization([2, 4, 7]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+
+
+
+
+
+
